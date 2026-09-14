f7ba6254c23cd9ff0258c31994479047769b8377
Servan <servank540@gmail.com>
2026-09-14 10:07:56 +0530

    Add files via upload

---
+++ float2.c
@@ -0,0 +1,6 @@
+#include<stdio.h>
+int main(){
+float n=12.89787;
+printf("%2f",n);
+return 0;
+}
---
+++ float4.c
@@ -0,0 +1,6 @@
+#include<stdio.h>
+int main(){
+float n=12.676856485;
+printf("%4f",n);
+return 0;
+}
